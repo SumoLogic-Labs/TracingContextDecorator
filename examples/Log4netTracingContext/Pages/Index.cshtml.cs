17e40fe0be6a3ba7c22bf267418cd474bf4d993e
Przemek Delewski <pdelewski@sumologic.com>
2023-12-01 15:38:32 +0100

    adding Log4netTracingContext example

---
+++ examples/Log4netTracingContext/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Log4netTracingContext.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(IndexModel));
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+	log.Info("OnGet");
+    }
+}
