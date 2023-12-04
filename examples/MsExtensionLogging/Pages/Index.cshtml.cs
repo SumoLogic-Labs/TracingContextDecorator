c7cadb0ae2b289a2e3e4e3481db13d5866341ecc
Przemek Delewski <pdelewski@sumologic.com>
2023-12-04 17:48:59 +0100

    add MsExtensionLogging example

---
+++ examples/MsExtensionLogging/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MsExtensionLogging.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+      _logger.LogInformation("About page visited at {DT}", 
+            DateTime.UtcNow.ToLongTimeString());
+    }
+}
